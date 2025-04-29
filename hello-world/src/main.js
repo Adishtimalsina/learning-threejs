c0b3b4632020774b5d0c0720707816f8a95261e9
adish timalsina <adishtimalsina11@gmail.com>
2025-04-28 19:03:30 -0500

    worked on roation, position, parent child mesh

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -6,16 +6,43 @@ const scene = new THREE.Scene();
 
 const cube = new THREE.BoxGeometry(1,1,1);
 
-const matrial = new THREE.MeshBasicMaterial({color:"red"})
+const matrial = new THREE.MeshBasicMaterial({color:"red", wireframe:true})
 
 const cubeMesh = new THREE.Mesh(cube, matrial)
 
+cubeMesh.position.y =1;
+
+//
+
+// //create a parent group, which holds multiple meshes
+// const cubeMesh1 = new THREE.Mesh(cube, matrial)
+// const cubeMesh2 = new THREE.Mesh(cube, matrial)
+
+// cubeMesh1.position.x = 2;
+// cubeMesh2.position.x= -2;
+
+// //create a group
+// const group = new THREE.Group();
+// group.add(cubeMesh, cubeMesh1, cubeMesh2)
+
+// //scale the parent, which will scale all the children meshes
+// group.scale.y = 2;
+
 scene.add(cubeMesh);
+
+
+
+//adding axises in the screen
+const axisesHelper = new THREE.AxesHelper(3);
+
+//adding axises helper to the mesh
+cubeMesh.add(axisesHelper)
+
+//adding axises helper to the scene
+//scene.add(axisesHelper);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const light = new THREE.AmbientLight({color:'white'})
-
-
 
 //position the camera
 camera.position.z = 5;
